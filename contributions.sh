491f43cc7cb23b4fe54e33ce5b7ab0ae6d12ca8f
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-19 16:46:46 +0530

    Update contributions.sh

--- contributions.sh
+++ contributions.sh
@@ -1,7 +1,7 @@
 REPO="Sopra-Banking-Software-Interns/Application-2.0"
 curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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"\
+  -H "Authorization: Bearer ${TOKEN}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/Sopra-Banking-Software-Interns/Application-2.0/contributors | jq -r '.[] | {login, contributions}' >> contributions.txt
 
