c21ae85a9312369f04351e6f6e3f6c84d53afd79
Doms <i@me.com>
2023-06-21 17:34:18 +0530

    Started issues

---
+++ issues.sh
@@ -0,0 +1,37 @@
+# curl -L \
+#   -H "Accept: application/vnd.github+json" \
+#   -H "Authorization: Bearer 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"\
+#   -H "X-GitHub-Api-Version: 2022-11-28" \
+#   https://api.github.com/user=Tushar-2510/issues
+
+#!/bin/bash
+
+OWNER="Sopra-Banking-Software-Interns"
+REPO="Github-Leaderboard"
+CONTRIBUTOR="Riyu44"
+ACCESS_TOKEN="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"
+
+# Make a request to fetch the contributor's information
+response=$(curl -s -L \
+   -H "Accept: application/vnd.github+json" \
+   -H "Authorization: Bearer $ACCESS_TOKEN" \
+   -H "X-GitHub-Api-Version: 2022-11-28" \
+     "https://api.github.com/repos/$OWNER/$REPO/issues?state=closed")
+
+echo $response | jq -r '[.[] | select( .user.login=="Tushar-2510") | .url] | length' 
+# | jq '.[]  | select( .state == "open" ) '
+
+# # Parse the response to find the contributor's information
+# contributor_info=$(echo "$response" | jq ".[] | select(.login == \"$CONTRIBUTOR\")")
+
+# # Check if the contributor exists in the repository
+# if [[ -z "$contributor_info" ]]; then
+#     echo "Contributor not found in the repository."
+#     exit 1
+# fi
+
+# # Get the number of issues solved by the contributor
+# issues_solved=$(echo "$contributor_info" | jq ".contributions")
+
+# # Print the number of issues solved by the contributor
+# echo "Number of issues solved by $CONTRIBUTOR: $issues_solved"
