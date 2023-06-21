5926bd704cd39f1df29fd59aa1788ca50a686f1c
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-21 10:04:36 +0530

    Update contributions.sh

--- contributions.sh
+++ contributions.sh
@@ -1,10 +1,10 @@
 #GET the contributors list from GIT Hub
-REPO="Sopra-Banking-Software-Interns/Application-2.0"
+REPO="Sopra-Banking-Software-Interns/Github-Learderboard"
 curl -s -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $token"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/Sopra-Banking-Software-Interns/Application-2.0/contributors | jq -r '.[] | {login, contributions}' >> contributions.txt
+  https://api.github.com/repos/Sopra-Banking-Software-Interns/Github-Leaderboard/contributors | jq -r '.[] | {login, contributions}' >> contributions.txt
 
 #Create a new game on the leaderboard on Cloud storage
 ID=$(curl -s -X POST -H "Content-Type: application/json" -d '{"name": "$REPO"}' https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ | jq -r '.result | scan("Game with ID: (.+) added.")[]')
